eaf2b82bd1493802e5a3d5b65c6a850a067f6a2a
Samy FEREDJ <samyferedj@outlook.fr>
2025-02-07 20:01:13 +0100

    maquette fonctionnelle

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const btn = document.getElementById("btn-no");
 const btnyes = document.getElementById("btn-yes");
 const width = window.innerWidth;
 const height = window.innerHeight;
-btn.style.top = "400px";
+btn.style.top = "250px";
+btnyes.style.top = "250px";
 btn.style.left = `${width/2.0 + 50}px`;
+btnyes.style.left = `${width/2.0 - 100}px`;
 btn.style.display = "block";
-
+btnyes.style.display = "block";
+//
+console.log("Window height : ", window.innerHeight);
+console.log("Window width : ", window.innerWidth);
+//
 btn.addEventListener("click", (e) => {
-  console.log(window.innerWidth, window.innerHeight);
-  console.log(btnyes.getBoundingClientRect());
-
   let buttonPosition = e.currentTarget.getBoundingClientRect();
-  const semiButtonWidth = (buttonPosition.right + buttonPosition.left) / 2.0;
-  const semiButtonHeight = (buttonPosition.top + buttonPosition.bottom) / 2.0;
-  const xRange = [semiButtonWidth, window.innerWidth - semiButtonWidth];
-  const yRange = [semiButtonHeight, window.innerHeight - semiButtonHeight];
+  const semiButtonWidth = (buttonPosition.right - buttonPosition.left) / 2.0;
+  const semiButtonHeight = (buttonPosition.top - buttonPosition.bottom) / 2.0;
+  const xRange = [semiButtonWidth + 0.1*window.innerWidth, window.innerWidth - (semiButtonWidth + 0.1*window.innerWidth)];
+  const yRange = [semiButtonHeight + 0.1*window.innerHeight, window.innerHeight - (semiButtonHeight + 0.1*window.innerHeight)];
   const newXPos = Math.random() * (xRange[1] - xRange[0]) + xRange[0];
   const newYPos =  Math.random() * (yRange[1] - yRange[0]) + yRange[0];
+  // debug
+  console.log(newXPos , newYPos);
+  btn.style.top = `${newYPos}px`;
+  btn.style.left = `${newXPos}px`;
 });
+
+btnyes.addEventListener("click", (e) => {
+  alert("OK ALORS PLUKETOU");
+  window.location.reload();
+})
